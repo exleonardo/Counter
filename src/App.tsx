104c9722d13d36cc904b505f52f3b5f9e0c04d7a
Artyom Korshykau <butovichjunior@gmail.com>
2023-07-18 00:57:53 +0300

    17.07.23

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import './App.css';
 import Button from "./components/Button";
 
 function App() {
 
-  let [inc,setInc]= useState<number>(0);
-  let minValue = 0;
-  let maxValue = 5;
+    let [inc, setInc] = useState<number>(0);
+    let minValue = 0;
+    let maxValue = 5;
 
-  const iteration = ()=>{
-    minValue++
-    setInc(++inc)
-  }
-  const resetInc = ()=>{
-  setInc(0)
+    const iteration = () => {
+        minValue++
+        setInc(++inc)
+    }
+    const resetInc = () => {
+        setInc(0)
+    }
+
+    return (
+        <div>
+            <div className={"App"}>
+                <div className={'Header'}><h1>{inc}</h1></div>
+                <div className={'buttons'}>
+                    <Button name={"inc"} callBack={iteration} hide={maxValue > inc}/>
+                    <Button name={"reset"} callBack={resetInc} hide={inc > minValue}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-  return (
-    <div className={"App"}>
-      <div className={'Header'}><h1>{inc}</h1></div>
-      <div className={'buttons'}>
-        <Button name={"inc"} callBack={iteration} hide={maxValue>inc}/>
-        <Button name={"reset"} callBack={resetInc}  hide={inc>minValue} />
-      </div>
-    </div>
-  );
-}
 export default App;
