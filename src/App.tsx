e074c5a88aba5550147dc88b722a7bbc92a746a4
Artyom Korshykau <butovichjunior@gmail.com>
2023-07-18 13:15:08 +0300

    add buttons module add img directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-
 import './App.css';
-import Button from "./components/Button";
+import Button from "./components/Buttons/Button";
+import s from "./components/Buttons/Button.module.css"
 
 function App() {
 
@@ -17,15 +17,15 @@ function App() {
         setInc(0)
     }
 
-  return (
-    <div className={"App"}>
-      <div className={maxValue>inc?'Header':"HeaderError"}><h1>{inc}</h1></div>
-      <div className={'buttons'}>
-        <Button name={"inc"} callBack={iteration} hide={maxValue>inc}/>
-        <Button name={"reset"} callBack={resetInc}  hide={inc>minValue} />
-      </div>
-    </div>
-  );
+    return (
+        <div className={"App"}>
+            <div className={maxValue > inc ? 'Header' : "HeaderError"}><h1>{inc}</h1></div>
+            <div className={s.buttons}>
+                <Button name={"inc"} callBack={iteration} hide={maxValue > inc}/>
+                <Button name={"reset"} callBack={resetInc} hide={inc > minValue}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from "./Button.module.css"
+
+type ButtonType = {
+    name: string
+    callBack: () => void
+    hide: boolean
+}
+const Button = (props: ButtonType) => {
+    const {name, callBack, hide} = props
+
+    return (
+
+        <button className={s.btn} onClick={callBack} disabled={!hide}>{name}</button>
+    );
+};
+
+export default Button;
