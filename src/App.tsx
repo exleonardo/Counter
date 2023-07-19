302558f7a66561d1073f9fd0b4b14d3ee6ee61f9
Exleonardo <exleonardo@mail.ru>
2023-07-19 22:58:00 +0300

    del changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, {useState} from "react";
+import React from "react";
 import './App.css';
-import Button from "./components/Counter/Buttons/Button/Button";
-import s from "./components/Counter/Buttons/Button/Button.module.css"
 import Counter from "./components/Counter/Counter";
 
 function App() {
