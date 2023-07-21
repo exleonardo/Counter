7f1b2100dba5add372bb2da995bbe6f2b9a50e27
Artyom Korshykau <butovichjunior@gmail.com>
2023-07-21 11:12:20 +0300

    Values

--- src/components/SettingsCounter/Values/MaxValue/MaxValue.tsx
+++ src/components/SettingsCounter/Values/MaxValue/MaxValue.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 const MaxValue = () => {
+
+    let [maxValue, setMaxValue] = useState<string | number>(0)
+
+    const addMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(e.currentTarget.value)
+    }
+
     return (
         <div>
             <div>Max value:</div>
-            <input type="number"/>
+            <input type="number" onChange={addMaxValue}/>
         </div>
     );
 };
--- src/components/SettingsCounter/Values/StartValue/StartValue.tsx
+++ src/components/SettingsCounter/Values/StartValue/StartValue.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 const StartValue = () => {
+
+    let [startValue, setStartValue] = useState(0)
+
+    const addStartValue = (e: ChangeEvent<HTMLInputElement>) => {
+        localStorage.setItem('startValue', e.currentTarget.value)
+    }
+
+
     return (
         <div>
             <div>Start value:</div>
-            <input type="number"/>
+            <input type="number" onChange={addStartValue}/>
         </div>
     );
 };
