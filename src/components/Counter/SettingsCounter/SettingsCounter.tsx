385b54f12a94b9044705cec6473bc9dbb9ec99f6
Artyom Korshykau <butovichjunior@gmail.com>
2023-07-21 15:06:43 +0300

    Settings css

--- src/components/Counter/SettingsCounter/SettingsCounter.tsx
+++ src/components/Counter/SettingsCounter/SettingsCounter.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import MaxValue from "./Values/MaxValue/MaxValue";
 import StartValue from "./Values/StartValue/StartValue";
 import Button from "../Button/Button";
+import s from '../SettingsCounter/SettingsButtons/SettingsCounter.module.css'
 
 
-type SettingsCounterType ={
-    getMinValue:(value:string)=>void;
-    getMaxValue:(value:string)=>void
-    callBack:()=>void;
-    setMinValue:string
-    setMaxValue:string
-    error:boolean
+type SettingsCounterType = {
+    getMinValue: (value: string) => void;
+    getMaxValue: (value: string) => void
+    callBack: () => void;
+    setMinValue: string
+    setMaxValue: string
+    error: boolean
 }
-const SettingsCounter = (props:SettingsCounterType) => {
-const {getMinValue,getMaxValue,callBack,setMinValue,setMaxValue,error} = props
+const SettingsCounter = (props: SettingsCounterType) => {
+    const {getMinValue, getMaxValue, callBack, setMinValue, setMaxValue, error} = props
 
     return (
-        <div>
+        <div className={s.settings}>
             <MaxValue getMaxValue={getMaxValue} setMaxValue={setMaxValue}/>
             <StartValue getMinValue={getMinValue} setValue={setMinValue}/>
             <Button name={"SET"} callBack={callBack} hide={!error}/>
